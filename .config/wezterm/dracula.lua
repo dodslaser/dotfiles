7990505f6847157f52493820983e07e85ca24e61
dodslaser <2969909+dodslaser@users.noreply.github.com>
2022-07-24 07:58:53 +0200

    Add WezTerm

---
+++ .config/wezterm/dracula.lua
@@ -0,0 +1,1 @@
+/home/erik/.submodules/dracula/wezterm/dracula.lua
---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,32 @@
+local dracula = require 'dracula'
+local wezterm = require 'wezterm'
+
+return {
+    font_size = 11,
+    font_antialias = "Subpixel",
+    font = wezterm.font_with_fallback {
+      'MesloLGS Nerd Font Mono',
+      'monospace'
+    },
+    colors = dracula,
+    enable_scroll_bar = true,
+    tab_bar_at_bottom = true,
+    use_fancy_tab_bar = false,
+    warn_about_missing_glyphs = false,
+    default_cursor_style = "BlinkingUnderline",
+    cursor_blink_rate = 600,
+    
+    keys = {
+        {
+            key = "d",
+            mods = "SUPER",
+            action = wezterm.action.SplitVertical { domain="CurrentPaneDomain" },
+        },
+        {
+            key = "d",
+            mods = "SUPER|SHIFT",
+            action = wezterm.action.SplitHorizontal {domain="CurrentPaneDomain"},
+        },
+    },
+}
+
